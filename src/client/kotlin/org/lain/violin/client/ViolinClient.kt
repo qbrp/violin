bc1187728c06331e6bb02da24b71422847f7c6ba
Трибунал <48561105+lain1wakura@users.noreply.github.com>
2025-11-01 08:01:08 +0300

    Инициализирован проект

---
+++ src/client/kotlin/org/lain/violin/client/ViolinClient.kt
@@ -0,0 +1,9 @@
+package org.lain.violin.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class ViolinClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
---
+++ src/main/kotlin/org/lain/violin/Violin.kt
@@ -0,0 +1,9 @@
+package org.lain.violin
+
+import net.fabricmc.api.ModInitializer
+
+class Violin : ModInitializer {
+
+    override fun onInitialize() {
+    }
+}
